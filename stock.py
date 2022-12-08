3c61030fa19d22eaa3ec8c7603dab35829822641
Elif Karagöz <70434376+mermaid1907@users.noreply.github.com>
2022-12-08 16:39:26 +0300

    Update stock.py

--- stock.py
+++ stock.py
@@ -4,7 +4,10 @@ import pandas as pd
 import yfinance as yf
 import plotly.graph_objects as go
 from datetime import date, timedelta
+import tensorflow_estimator as tf
 from sklearn.model_selection import train_test_split
+from keras.models import Sequential
+from keras.layers import Dense, LSTM
 
 today = date.today().strftime("%Y-%m-%d")
 end_date = today
@@ -31,7 +34,21 @@ for stock_code in stock_list:
     
     xtrain, xtest, ytrain, ytest = train_test_split(x, y, test_size=0.3, random_state=10) 
     
+    model = Sequential()
     
+    model.add(LSTM(128, return_sequences=True, input_shape= (xtrain.shape[1], 1)))
+    model.add(LSTM(64, return_sequences=False))
+    model.add(Dense(25))
+    model.add(Dense(1))
+    model.summary()
+    
+    model.compile(optimizer='adam', loss='mean_squared_error')
+    model.fit(xtrain, ytrain, batch_size=1, epochs=30)
+    
+    #features = [Open, High, Low, Adj Close, Volume]
+    features = np.array([data["Open"], data["High"], data["Low"], data["Volume"]])
+    print(model.predict(features))
+        
     
     
     
