14594d9765ba4a2a0bb5f64711a5a697f6bce766
Elif Karagöz <70434376+mermaid1907@users.noreply.github.com>
2022-12-13 11:12:31 +0300

    Update gui.py

--- GUI/gui.py
+++ GUI/gui.py
@@ -5,17 +5,13 @@ import numpy as np
 # when you click the button
 def button_command():
     # get() method gets the text that you write in the entry box
-    var1 = int(entry1.get())
-    var2 = int(entry2.get())
-    var3 = int(entry3.get())
+    var1 = entry1.get()
 
-    '''
-    #calling module example:
+    #calling module 
     matrix = ansys_project.main(var1, var2, var3)
 
     # insert() method send the result to Textbox
     output.insert(END, matrix)
-    '''
     return None
     
 def gui(window_title):
@@ -24,28 +20,17 @@ def gui(window_title):
     # root.geometry('400x350')
     root.title(window_title)
 
-    text1 = Label(root, text="First Input", width=17)
+    text1 = Label(root, text="Stock Code", width=17)
     text1.pack()
     global entry1
     entry1 = Entry(root, width=20)
     entry1.pack()
 
-    text2 = Label(root, text="Second Input", width=17)
-    text2.pack()
-    global entry2
-    entry2 = Entry(root, width=20)
-    entry2.pack()
-
-    text3 = Label(root, text="Third Input", width=17)
-    text3.pack()
-    global entry3
-    entry3 = Entry(root, width=20)
-    entry3.pack()
 
     spacer1 = Label(root, text="")
     spacer1.pack()
     # we are calling button_command function in Button
-    Button(root, text="Calculate", width=17, command=button_command).pack()
+    Button(root, text="Predict", width=17, command=button_command).pack()
     spacer2 = Label(root, text="")
     spacer2.pack()
 
@@ -63,4 +48,4 @@ def gui(window_title):
 
 
 if __name__ == "__main__":
-    gui("Stock Price Pred. (LSTM)")
+    gui("Stock Price Pred.")
